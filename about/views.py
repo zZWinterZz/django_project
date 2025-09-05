20e5748af8a7cfdb8d6dff8d050253699268b11e
zZWinterZz <ryanwinter1989@gmail.com>
2025-09-05 16:13:52 +0100

    Add about page view and update URL configuration

--- about/views.py
+++ about/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+def about_me(request):
+    return HttpResponse("This would be the about page")
 # Create your views here.
